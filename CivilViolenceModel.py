79d2b89c4a7852bd8ceb6dbf5a42d356ae534750
Lennart Faber <lennart@klippa.com>
2019-09-17 15:15:13 +0200

    more basic setup

--- CivilViolenceModel.py
+++ CivilViolenceModel.py
@@ -6,7 +6,26 @@ class PopulationAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
 
+    def step(self):
+        # The agent's step will go here.
+        pass
+
+
 class CopAgent(Agent):
     """An agent that can arrest PopulationAgents."""
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
+
+    def step(self):
+        # The agent's step will go here.
+        pass
+
+class CivilViolenceModel(Model):
+    """A model with some number of PopulationAgents and some number of CopAgents."""
+    def __init__(self, nPopulation, nCops):
+        self.num_population = nPopulation
+        self.num_cops = nCops
+
+    def step(self):
+        """Advance the model by one step."""
+        self.schedule.step()
