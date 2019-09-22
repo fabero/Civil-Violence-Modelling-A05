d7bdac9381bdf43244cfd0e201097f2944b8f7af
gtziafas <pullmypill@hotmail.com>
2019-09-22 04:07:43 +0300

    Update CivilViolenceServer.py

--- CivilViolenceServer.py
+++ CivilViolenceServer.py
@@ -41,7 +41,7 @@ model_params =  {"citizen_density": UserSettableParameter("slider", "Citizen Den
                                                         description="Number of patches visible to cops"),
                         "citizen_vision": UserSettableParameter("slider", "Citizen Vision", 7, 0, 10,
                                                         description="Number of patches visible to citizens"),
-                        "legitimacy": UserSettableParameter("slider", "Government Legitimacy", 80, 0, 100,
+                        "legitimacy": UserSettableParameter("slider", "Government Legitimacy", 82, 0, 100,
                                                         description="Global parameter: Government legitimacy"),
                         "max_jail_term": UserSettableParameter("slider", "Max Jail Term", 30, 0, 1000,
                                                         description="Maximum number of steps that jailed citizens stay in" ),
