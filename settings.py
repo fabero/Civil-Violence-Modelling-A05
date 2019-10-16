9c1e4de10c3eec1efd9508ed3dcf9f76328d6b11
gtziafas <pullmypill@hotmail.com>
2019-10-16 18:10:30 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
+import numpy as np
+
 POPULATION_AGENT_CLASS = 'population'
 PROPAGANDA_AGENT_CLASS = 'propaganda'
 COP_AGENT_CLASS = 'cop'
+
+def tuned_sigmoid(z, alpha=1, clip=1):
+	return 1 / (1 + np.exp( - alpha * ( 12/(1+clip)*z-6 )))
